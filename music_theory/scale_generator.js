86a5c59cf22582234a0b659b53062b2b98d4d17b
Anonoei <contact@anonoei.com>
2022-05-22 01:58:51 -1000

    Scale generator extensibility
    Moved canvas attributes to .js
    Added ScalePattern comment
    Added Slider for height and width

--- music_theory/scale_generator.js
+++ music_theory/scale_generator.js
@@ -2,13 +2,8 @@ let KeyCountWhite = 22;
 let KeyCountBlack = 15;
 let KeyCount = 37;
 
-let CanvasHeight = 150;
-let CanvasWidth = 660;
-
-let KeyWidthWhite = CanvasWidth / KeyCountWhite;
-let KeyHeightWhite = CanvasHeight;
-let KeyWidthBlack = KeyWidthWhite * .5;
-let KeyHeightBlack = CanvasHeight * (.66);
+let CanvasHeight = 100;
+let CanvasWidth = 500;
 
 let KeyLabels = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
 let LabelStandard = ["C", "D", "E", "F", "G", "A", "B"]
@@ -16,36 +11,71 @@ let LabelSharp = ["B#", "C#", "D#", "E#", "F#", "G#", "A#"]
 let LabelFlat = ["D♭", "E♭", "F♭", "G♭", "A♭", "B♭", "C♭"]
 
 let KeyColors = "wbwbwwbwbwbw"
-let KeyWhite = []
-let KeyBlack = []
-let KeyPos = [];
-for (let i = 0; i < KeyCount; i++)
+
+// Sliders
+let widthMin = "420";
+let widthMax = "1440";
+var width_slider = document.getElementById("width_slider");
+width_slider.setAttribute("min", widthMin);
+width_slider.setAttribute("max", widthMax);
+width_slider.setAttribute("value", CanvasWidth.toString());
+
+width_slider.oninput = function()
 {
-    if (KeyColors[i % 12] == "w")
-    {
-        KeyPos.push(KeyWidthWhite * KeyWhite.length)
-        KeyWhite.push(i)
-    }
-    else
-    {
-        KeyPos.push(KeyPos[i - 1] + (KeyWidthWhite - KeyWidthBlack/2))
-        KeyBlack.push(i)
-    }
+    CanvasWidth = this.value;
+    Draw.call(this);
 }
 
-let ScaleKeys = [ScaleKey];
-for (let i = 1; i < 15; i++)
+let heightMin = "50";
+let heightMax = "200";
+var height_slider = document.getElementById("height_slider");
+height_slider.setAttribute("min", heightMin);
+height_slider.setAttribute("max", heightMax);
+height_slider.setAttribute("value", CanvasHeight.toString());
+
+height_slider.oninput = function()
 {
-    if (ScalePattern[(i - 1) % 7] == "f")
-    {
-        ScaleKeys.push(ScaleKeys[i-1] + 2)
-    }
-    else
-    {
-        ScaleKeys.push(ScaleKeys[i-1] + 1)
-    }
+    CanvasHeight = this.value;
+    Draw.call(this);
 }
 
+function Initialize()
+{
+    KeyWidthWhite = CanvasWidth / KeyCountWhite;
+    KeyHeightWhite = CanvasHeight;
+    KeyWidthBlack = KeyWidthWhite * .5;
+    KeyHeightBlack = CanvasHeight * (.66);
+
+    KeyWhite = []
+    KeyBlack = []
+    KeyPos = [];
+    for (let i = 0; i < KeyCount; i++)
+    {
+        if (KeyColors[i % 12] == "w")
+        {
+            KeyPos.push(KeyWidthWhite * KeyWhite.length)
+            KeyWhite.push(i)
+        }
+        else
+        {
+            KeyPos.push(KeyPos[i - 1] + (KeyWidthWhite - KeyWidthBlack/2))
+            KeyBlack.push(i)
+        }
+    }
+
+    ScaleKeys = [ScaleKey];
+    for (let i = 1; i < 15; i++)
+    {
+        if (ScalePattern[(i - 1) % 7] == "f")
+        {
+            ScaleKeys.push(ScaleKeys[i-1] + 2)
+        }
+        else
+        {
+            ScaleKeys.push(ScaleKeys[i-1] + 1)
+        }
+    }
+}
 
 function Draw()
 {
@@ -54,32 +84,47 @@ function Draw()
         alert("Required variables are not defined!\nPlease define them and try again.");
         return;
     }
+    Initialize.call(this);
     // Scale
-    var ctx = document.getElementById("scale").getContext("2d");
+    var canvas = document.getElementById("scale");
+    InitializeCanvas.call(this, canvas);
+    ctx = canvas.getContext("2d")
     DrawKeyboard.call(this, ctx);
     DrawScaleLabels.call(this, ctx)
     // Chord 1
-    var ctx = document.getElementById("chord1").getContext("2d");
+    var canvas = document.getElementById("chord1");
+    InitializeCanvas.call(this, canvas);
+    ctx = canvas.getContext("2d")
     DrawKeyboard.call(this, ctx);
     DrawChordLabels.call(this, ctx, [1,3,5,7])
     // Chord 2
-    var ctx = document.getElementById("chord2").getContext("2d");
+    var canvas = document.getElementById("chord2");
+    InitializeCanvas.call(this, canvas);
+    ctx = canvas.getContext("2d")
     DrawKeyboard.call(this, ctx);
     DrawChordLabels.call(this, ctx, [2,4,6,8])
     // Chord 3
-    var ctx = document.getElementById("chord3").getContext("2d");
+    var canvas = document.getElementById("chord3");
+    InitializeCanvas.call(this, canvas);
+    ctx = canvas.getContext("2d")
     DrawKeyboard.call(this, ctx);
     DrawChordLabels.call(this, ctx, [3,5,7,9])
     // Chord 4
-    var ctx = document.getElementById("chord4").getContext("2d");
+    var canvas = document.getElementById("chord4");
+    InitializeCanvas.call(this, canvas);
+    ctx = canvas.getContext("2d")
     DrawKeyboard.call(this, ctx);
     DrawChordLabels.call(this, ctx, [4,6,8,10])
     // Chord 5
-    var ctx = document.getElementById("chord5").getContext("2d");
+    var canvas = document.getElementById("chord5");
+    InitializeCanvas.call(this, canvas);
+    ctx = canvas.getContext("2d")
     DrawKeyboard.call(this, ctx);
     DrawChordLabels.call(this, ctx, [5,7,9,11])
     // Chord 6
-    var ctx = document.getElementById("chord6").getContext("2d");
+    var canvas = document.getElementById("chord6");
+    InitializeCanvas.call(this, canvas);
+    ctx = canvas.getContext("2d")
     DrawKeyboard.call(this, ctx);
     DrawChordLabels.call(this, ctx, [6,8,10,12])
 }
@@ -110,36 +155,44 @@ function DrawKeyboard(ctx)
 
 function DrawScaleLabels(ctx)
 {
-    ctx.fillStyle = "#888888";
+    ctx.font = "bold 16px arial";
+    ctx.fillStyle = "#66AAEE";
+    ctx.textAlign = "center";
     for (let index of ScaleKeys)
     {
-        if (KeyColors[index % 12] == "w")  // Drawing white label
+        if (KeyColors[index % 12] == "w")  // Drawing white key label
         {
-        ctx.font = "20px serif";
-        ctx.fillText(KeyLabels[index % 12], KeyPos[index] + (KeyWidthWhite/4), CanvasHeight - 10);
+        ctx.fillText(KeyLabels[index % 12], KeyPos[index] + (KeyWidthWhite/2), CanvasHeight - 5);
         }
         else
         {
-        ctx.font = "16px serif";
-        ctx.fillText(KeyLabels[index % 12], KeyPos[index] - 2, KeyHeightBlack - 10);
+        ctx.fillText(KeyLabels[index % 12], KeyPos[index] + (KeyWidthBlack/2), KeyHeightBlack - 5);
         }
     }
 }
 
 function DrawChordLabels(ctx, keyIndexes)
 {
-    ctx.fillStyle = "#888888";
+    ctx.font = "bold 16px arial";
+    ctx.fillStyle = "#66AAEE";
+    ctx.textAlign = "center";
     for (let index of keyIndexes)
     {
         if (KeyColors[ScaleKeys[index] % 12] == "w")
         {
-        ctx.font = "20px serif";
-        ctx.fillText(KeyLabels[ScaleKeys[index] % 12], KeyPos[ScaleKeys[index]] + (KeyWidthWhite/4), CanvasHeight - 10)
+        
+        ctx.fillText(KeyLabels[ScaleKeys[index] % 12], KeyPos[ScaleKeys[index]] + (KeyWidthWhite/2), CanvasHeight - 5)
         }
         else
         {
-        ctx.font = "16px serif";
-        ctx.fillText(KeyLabels[ScaleKeys[index] % 12], KeyPos[ScaleKeys[index]] - 2, KeyHeightBlack - 10)
+        ctx.fillText(KeyLabels[ScaleKeys[index] % 12], KeyPos[ScaleKeys[index]] + (KeyWidthBlack/2), KeyHeightBlack - 5)
         }
     }
+}
+
+function InitializeCanvas(canvas)
+{
+    canvas.setAttribute("width", CanvasWidth.toString())
+    canvas.setAttribute("height", CanvasHeight.toString())
+    canvas.setAttribute("style", "border:1px solid #c3c3c3;")
 }
